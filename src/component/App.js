11c3a253c3301191de740c37cd8527ab0dc89f77
rappad <wasuwat.se@gmail.com>
2019-06-25 22:54:46 +0700

    init

--- src/component/App.js
+++ src/component/App.js
@@ -5,6 +5,12 @@ export default class App extends Component {
     return (
       <main>
         <h1>Hello world</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+          assumenda provident reprehenderit corrupti officiis nemo commodi
+          veritatis atque id, autem, voluptatem pariatur tempora inventore quia!
+          Deserunt, debitis suscipit. Debitis, doloribus.
+        </p>
       </main>
     )
   }
